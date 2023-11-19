998af0809ab7b1a551c4bc4b789db14eb2236ab4
Matias Salimbene <msalimbene@homelendingpal.com>
2023-11-19 10:48:41 -0300

    my commit

--- client/index.js
+++ client/index.js
@@ -1,17 +1,21 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
+
+const merkleTree = new MerkleTree(niceList);
+const name = process.argv[2];
+const index = niceList.findIndex((n) => n === name);
+const proof = merkleTree.getProof(index);
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
+  // prove to the server we're on the nice list
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
+    proof,
+    name,
   });
 
   console.log({ gift });
 }
-
 main();
